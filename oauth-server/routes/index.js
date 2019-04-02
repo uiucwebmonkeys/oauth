d1d272af1caa0c697930342169ca7be4f0b04bf2
Jonathan Xu <jonathanj.xu97@gmail.com>
2019-04-02 12:19:03 -0500

    Backend adjustments

--- oauth-server/routes/index.js
+++ oauth-server/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.json({ message: 'hello' });
+});
+
+router.get('/info', (req, res, next) => {
+  res.json({ params: req.query });
 });
 
 module.exports = router;
